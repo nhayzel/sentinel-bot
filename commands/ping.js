0b372cf86659974ab8354b158a9e07ea29d9621f
nhayzel <97143856+nhayzel@users.noreply.github.com>
2022-01-09 13:28:08 -0500

    Clearing the Repo

--- commands/ping.js
+++
@@ -1,9 +0,0 @@
-module.exports =
-{
-    name: 'ping',
-    description:  "This is the ping command. The bot will respond with one line of text, confirming it is online.",
-    execute(message, args)
-    {
-        message.channel.send('Pong. Sentinel is online and running.');
-    }
-}
